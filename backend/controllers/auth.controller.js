b81e470d15e68f54f44afe95cc653fbde57567ca
Samyak Jain <samyaksk10@gmail.com>
2025-01-14 17:28:28 +0530

    done with signup and login functionalities

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,66 +1,92 @@
 import User from "../models/user.model.js";
-
-export const loginUser = async(req,res) =>{
- console.log("Login User");
- res.send("Login user")
- 
-}
+import bcrypt from "bcryptjs";
+import generateTokenAndSetCookie from "../utils/generateToken.js";
 
 
 
-export const signupUser= async(req,res) =>{
-    try {
-        const {fullname,username,password,confirmPassword,gender} = req.body;
-        
-        if(password !== confirmPassword)
-        {
-            return res.status(400).json({
-                error:"Passwords doesn't match",
+export const signupUser = async (req, res) => {
+  try {
+    const { fullName, userName, password, confirmPassword, gender } = req.body;
 
-            })
-        }
-        
-        const user = await User.findOne({username});
-        if(user)
-        {
-            return res.status(400).json({
-                error:"Username already exists",
-
-            })
-        }
-        
-        // hashing password
-
-
-        const boyProfilePic= `https://avatar.iran.liara.run/public/boy?username=${username}`
-        const girlProfilePic= `https://avatar.iran.liara.run/public/girl?username=${username}`
-         
-        const newUser = new User({
-           fullname,
-           username,
-           password,
-           gender,
-           profilePic : gender==="male"? boyProfilePic : girlProfilePic
-        })
-        
-        await newUser.save();
-        
-        
-        
-    } catch (error) {
-        res.status(500).send({
-            message:"Internal Error",
-            error
-        })
+    if (password !== confirmPassword) {
+      return res.status(400).json({
+        error: "Passwords doesn't match",
+      });
     }
 
+    const user = await User.findOne({ userName });
+    if (user) {
+      return res.status(400).json({
+        error: "Username already exists",
+      });
+    }
 
-}
+    // hashing password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
 
+    const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${userName}`;
+    const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${userName}`;
 
+    const newUser = new User({
+      fullName,
+      userName,
+      password: hashedPassword,
+      gender,
+      profilePic: gender === "male" ? boyProfilePic : girlProfilePic,
+    });
 
-export const logoutUser = async(req,res) =>{
-    console.log("Logout user");
-    res.send("Logout user")
+    if (newUser) {
+      // jwt token generation
+      generateTokenAndSetCookie(newUser._id, res);
 
-}
+      await newUser.save();
+
+      res.status(201).json({
+        _id: newUser._id,
+        fullName: newUser.fullName,
+        profilePic: newUser.profilePic,
+      });
+    } else {
+      res.status(400).json({ error: "Invalid User data" });
+    }
+  } catch (error) {
+    return res.status(500).json({
+      message: "Internal server Error",
+      error,
+    });
+  }
+};
+
+export const loginUser = async (req, res) => {
+    try {
+      const { userName, password } = req.body;
+      const user = await User.findOne({ userName });
+      const isPasswordCorrect = await bcrypt.compare(
+        password,
+        user?.password || ""
+      );
+  
+      if (!user || !isPasswordCorrect) {
+        return res.status(400).json({ error: "Invalid username or password" });
+      }
+  
+      generateTokenAndSetCookie(user._id, res);
+  
+      res.status(200).json({
+        _id: user._id,
+        fullName: user.fullName,
+        userName: user.userName,
+        profilePic: user.profilePic,
+      });
+  
+    } catch (error) {
+      console.log("Error in login controller", error.message);
+      res.status(500).json({ error: "Internal Server Error" });
+    }
+  };
+
+export const logoutUser = async (req, res) => {
+  console.log("Logout user");
+  res.send("Logout user");
+};
