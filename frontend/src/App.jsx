0a643ca35c468f196218f97ff36adc04afa0e3bf
Samyak Jain <samyaksk10@gmail.com>
2025-01-16 23:05:29 +0530

    started with homepage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,14 @@
 import './App.css'
-import Login from './pages/login/Login.jsx'
+// import Login from './pages/login/Login.jsx'
+// import Signup from './pages/signup/Signup.jsx'
+import Home from './pages/home/Home.jsx'
 function App() {
 
   return (
     <>
        <div className='p-4 h-screen flex items-center justify-center'> 
-          <Login/>
+          {/* <Signup/> */}
+          <Home/>
        </div>
         </>
   )
